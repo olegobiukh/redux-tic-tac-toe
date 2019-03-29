3dbba2ad9aa3b5c65d6f4a4ffd26e74968878762
obiukh19 <oleg.obiukh@gmail.com>
2019-03-29 14:59:12 +0200

    Deploy 1

---
+++ build/precache-manifest.0acb96b4c61dcf807f1ff29216b2ff66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/redux-tic-tac-toe/build/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6477471ef7eb53d4c9f6",
+    "url": "/redux-tic-tac-toe/build/static/js/runtime~main.cc7ad276.js"
+  },
+  {
+    "revision": "e2e2afa2b2dcdec7cb6a",
+    "url": "/redux-tic-tac-toe/build/static/js/main.059776f7.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/redux-tic-tac-toe/build/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "e2e2afa2b2dcdec7cb6a",
+    "url": "/redux-tic-tac-toe/build/static/css/main.b2afe20e.chunk.css"
+  },
+  {
+    "revision": "f505b767521c4155e116b204fae60394",
+    "url": "/redux-tic-tac-toe/build/index.html"
+  }
+];
