9a1e6f14a8f08a398ac7f24773d7ec4e8fee62d0
obiukh19 <oleg.obiukh@gmail.com>
2019-04-01 19:22:04 +0300

    added some core code

--- src/App.js
+++ src/App.js
@@ -1,73 +1,25 @@
 import React from "react";
-import { connect } from "react-redux";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-class App extends React.Component {
-  render() {
-    const { items, winner, counter } = this.props;
-    const { setItems, getRestarted } = this.props;
+import Child from "./Child";
+import config from "./config";
 
-    return (
-      <div className="App">
-        <div
-          className={`Heading ${(winner || counter === 9) && "Heading__box"}`}
-        >
-          <h1 className={`${(winner || counter === 9) && "Heading_End"}`}>
-            {winner
-              ? `${winner} won`
-              : counter !== 9 && counter % 2 === 0
-              ? "X"
-              : counter !== 9 && counter % 1 === 0
-              ? "O"
-              : "Draw"}
-          </h1>
-        </div>
-        <div className={`Game ${(winner || counter === 9) && "End"}`}>
-          {items.map((item, i) => {
-            if (item) {
-              return (
-                <span className="Box Box__checked" key={i}>
-                  {item}
-                </span>
-              );
-            }
-            return (
-              <span className="Box" key={i} onClick={() => setItems(i)}>
-                {item}
-              </span>
-            );
-          })}
-        </div>
-        <button className="Button" onClick={() => getRestarted()}>
-          Restart
-        </button>
+function App() {
+  return (
+    <Router>
+      <div className="container">
+        <ul>
+          {config.headers.map(header => (
+            <li key={header}>
+              <Link to={`/${header}`}>{header}</Link>
+            </li>
+          ))}
+        </ul>
+
+        <Route exact path="/:id" component={Child} />
       </div>
-    );
-  }
+    </Router>
+  );
 }
 
-const mapStateToProps = state => ({
-  items: state.items,
-  counter: state.counter,
-  winner: state.winner,
-  restart: state.restart
-});
-
-const mapDispatchToProps = dispatch => ({
-  setItems(index) {
-    dispatch({
-      type: "SET_ITEMS",
-      index
-    });
-  },
-  getRestarted(bool) {
-    dispatch({
-      type: "RESTART",
-      restart: true
-    });
-  }
-});
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App);
+export default App;
---
+++ src/config.js
@@ -0,0 +1,88 @@
+export default {
+  headers: ["home", "about", "works", "contact"],
+  child: {
+    home: [
+      {
+        text: "gfhdfh"
+      }
+    ],
+    about: [
+      {
+        title: "about",
+        text:
+          "I am a result and process oriented person with strong theoretical background and good practical experience in Web development. I have extensive knowledge of HTML/CSS, Javascript and React. I am thorough and precise in everything I do, and have a keen interest in technology, mobile applications and user experience."
+      },
+      {
+        title: "technologies",
+        tech: ["html", "css // responsive", "js // ES6", "react // redux"]
+      }
+    ],
+    works: [
+      {
+        title: "projects",
+        projects: [
+          {
+            name: "FE1",
+            src: "https://olegobiukh.github.io/FE/1/"
+          },
+          {
+            name: "FE2",
+            src: "https://olegobiukh.github.io/FE/2/"
+          },
+          {
+            name: "FE3",
+            src: "https://olegobiukh.github.io/FE/2/"
+          },
+          {
+            name: "Carousel",
+            src: "https://jsfiddle.net/olegobiukh/k2qjoxm1/7/"
+          },
+          {
+            name: "Sliding tree",
+            src: "https://jsfiddle.net/olegobiukh/s52t8w9n/3/"
+          },
+          {
+            name: "Move the ball",
+            src: "https://jsfiddle.net/olegobiukh/edofkj8n/"
+          },
+          {
+            name: "Vouter",
+            src: "https://jsfiddle.net/olegobiukh/dajpLz64/1/"
+          },
+          {
+            name: "Tabs",
+            src: "https://jsfiddle.net/olegobiukh/yenh2vsk/1/"
+          },
+          {
+            name: "Gallery",
+            src: "https://jsfiddle.net/olegobiukh/6nhw42mz/1/"
+          },
+          {
+            name: "Star Wars",
+            src: "https://olegobiukh.github.io/react-sw/"
+          },
+          {
+            name: "To do app",
+            src: "https://olegobiukh.github.io/react-todo-list/"
+          },
+          {
+            name: "Tic Tac Toe react",
+            src: "https://olegobiukh.github.io/react-tic-tac-toe/"
+          },
+          {
+            name: "Data Table",
+            src: "https://olegobiukh.github.io/react-data-table/"
+          },
+          {
+            name: "Dropdown",
+            src: "https://olegobiukh.github.io/react-dropdown/"
+          },
+          {
+            name: "Tic Tac Toe redux",
+            src: "https://olegobiukh.github.io/redux-tic-tac-toe/"
+          }
+        ]
+      }
+    ]
+  }
+};
